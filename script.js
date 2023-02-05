07415d5a1411d7db9959ce68088074a6ece7fd69
yashita <yashitakhandelwal12@gmail.com>
2023-02-05 23:38:20 +0530

    add exisiting file to git

---
+++ script.js
@@ -0,0 +1,75 @@
+var email=document.getElementById("Email");
+var username =document.getElementById("userName");
+var password=document.getElementById("passWord");
+var number=document.getElementById("phno");
+var form=document.getElementById("form");
+
+form.addEventListener('submit',e=>{
+     e.preventDefault();
+     validateInputs();
+     window.location.replace("login.html");
+
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const numberValue = number.value.trim();
+
+    if(usernameValue === '') {
+        setError(username, 'Username is required');
+    } else {
+        setSuccess(username);
+    }
+
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+
+    if(passwordValue === '') {
+        setError(password, 'Password is required');
+    } else if (passwordValue.length < 8 ) {
+        setError(password, 'Password must be at least 8 character.')
+    } else {
+        setSuccess(password);
+    }
+
+    if(numberValue === '') {
+        setError(number, 'number is required');
+    } else if (numberValue.length < 10) {
+        setError(number, 'number must be at least 10 character.')
+    } else {
+        setSuccess(number);
+    }
+};
+
+
+
