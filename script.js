af3b6719f5608c683c67ab81670a8b371db50d0a
yashita <yashitakhandelwal12@gmail.com>
2023-02-05 23:55:05 +0530

    add eisting files to git

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ var form=document.getElementById("form");
 form.addEventListener('submit',e=>{
      e.preventDefault();
      validateInputs();
+     if(flag1==true && flag2==true && flag3==true && flag4==true){
      window.location.replace("login.html");
-
+     }
 });
 
 const setError = (element, message) => {
@@ -42,31 +43,42 @@ const validateInputs = () => {
 
     if(usernameValue === '') {
         setError(username, 'Username is required');
+        flag1=false;
     } else {
+        flag1=true;
         setSuccess(username);
     }
 
     if(emailValue === '') {
         setError(email, 'Email is required');
+        flag2=false;
     } else if (!isValidEmail(emailValue)) {
         setError(email, 'Provide a valid email address');
+        flag2=false;
     } else {
+        flag2=true;
         setSuccess(email);
     }
 
     if(passwordValue === '') {
         setError(password, 'Password is required');
+        flag3=false;
     } else if (passwordValue.length < 8 ) {
-        setError(password, 'Password must be at least 8 character.')
+        setError(password, 'Password must be at least 8 character.');
+        flag3=false;
     } else {
+        flag3=true;
         setSuccess(password);
     }
 
     if(numberValue === '') {
         setError(number, 'number is required');
+        flag4=false;
     } else if (numberValue.length < 10) {
-        setError(number, 'number must be at least 10 character.')
+        setError(number, 'number must be at least 10 character.');
+        flag4=false;
     } else {
+        flag4=true;
         setSuccess(number);
     }
 };
